dffdc0f56636c0fbf2c89b860c8d6ced64b8c59c
Jaime <jaimeloeuf@gmail.com>
2021-05-26 19:54:08 +0800

    [web] Fix issues with vetur/prettier/eslint: 1.  Add vetur.config.js in root to allow vetur plugin to find web/ repo's     package.json for a monorepo layout like this repo. 2.  Add jsconfig.json to web/ for vetur to find the source files. 3.  Update eslint config to add some missing configs. 4.  Update eslint's vue plugin version.

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -5,12 +5,21 @@ module.exports = {
   root: true,
   env: {
     node: true,
+    browser: true,
+    es2021: true,
   },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint",
+    ecmaVersion: 12,
+    sourceType: "module",
   },
+  plugins: ["vue"],
   rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
     "no-unused-vars": errorOnProduction,
     "no-debugger": errorOnProduction,
     "comma-dangle": ["error", "only-multiline"],
