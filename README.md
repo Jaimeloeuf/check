4eea10c3150002a147dff9f60837599fb5743138
Jaime <jaimeloeuf@gmail.com>
2023-10-06 22:19:32 +0800

    Clean up README and vetur config and add LICENSE.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# Checklist
+# Check
 A SaaS for checklists.
 
+
 ## Project structure
-- web
-    - Project directory for the webapp made with Vue/Bulma
+- app
+    - Project directory for the webapp
 - server
-    - Project directory for the API server made with ExpressJS
+    - Project directory for the API server
+
 
 ## LICENSE / Author / Contributing
 AGPL License, made by [JJ](https://github.com/Jaimeloeuf), contribute however you like!
--- vetur.config.js
+++ vetur.config.js
@@ -7,7 +7,7 @@ module.exports = {
   // Notice: It only affects the settings used by Vetur.
   settings: {
     "vetur.useWorkspaceDependencies": true,
-    "vetur.experimental.templateInterpolationService": true
+    "vetur.experimental.templateInterpolationService": true,
   },
 
   // **optional** default: `[{ root: './' }]`
@@ -17,13 +17,7 @@ module.exports = {
       // **required**
       // Where is your project?
       // It is relative to `vetur.config.js`.
-      root: './web',
-
-      // **optional** default: `'package.json'`
-      // Where is `package.json` in the project?
-      // We use it to determine the version of vue.
-      // It is relative to root property.
-      package: './package.json',
-    }
-  ]
-}
+      root: "./app",
+    },
+  ],
+};
